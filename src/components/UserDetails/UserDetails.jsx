41330da925deab01c9a5fafad9a525d7a39a4f3c
AsifHossain22 <only1asif4you22@gmail.com>
2025-09-26 17:30:22 +0400

    Explored Hooks: useNavigate and useNavigator for router spinner

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useParams } from "react-router";
 
 const UserDetails = () => {
+  const { userId } = useParams();
+  console.log(userId);
+
+  //   const params = useParams();
+  //   console.log(params.userId);
+
   return (
     <div>
       <h3>User Details</h3>
